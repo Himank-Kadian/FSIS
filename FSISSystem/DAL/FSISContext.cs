068a46eae266ce93679eb957d4622966bb841d0d
Himank Kadian <himankkadian@gmail.com>
2020-03-09 11:29:27 -0600

    Doesn't work yet

---
+++ FSISSystem/DAL/FSISContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSISSystem.DAL
+{
+    internal class FSISContext : DBContext
+    {
+        public FSISContext() : base("FSIS")
+        {
+
+        }
+        public FSISSet<Guardian> Regions { get; set; }
+        public FSISSet<Player> Player { get; set; }
+        public FSISSet<Team> Team { get; set; }
+    }
+}
